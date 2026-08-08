b67f030b2ce325c7539dfcca10873855e3106544
AdityaNjr510 <51448754+AdityaNjr510@users.noreply.github.com>
2026-08-08 15:41:31 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        hashmap1 = collections.defaultdict(int)
+        hashmap2 = collections.defaultdict(int)
+
+        for i in s:
+            hashmap1[i] += 1
+
+        for i in t:
+            hashmap2[i] += 1
+        
+        return hashmap1 == hashmap2
